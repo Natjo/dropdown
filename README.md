3541147c41ffd05cbfdfebff0c0651a87d295ef5
Jonathan MARTIN <j.martin@lonsdale.fr>
2020-12-19 20:11:25 +0100

    base

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Dropdown
+
+![version](https://img.shields.io/github/manifest-json/v/Natjo/dropdown)
+
+
+
+## Usage
+
+### html
+```html
+<button type="button" class="btn-image">See</button>
+<template>
+	<img src="https://picsum.photos/id/241/800/1400" alt="" loading="lazy" width="800" height="1400">
+</template>
+```
+### javascript
+```javascript
+import dropdown from './modules/dropdown/dropdown';
+dropdown();
+```
+
+## Demo
+[See codepen demo](https://codepen.io/natjo/pen/ZZrBMg?editors=0011)
---
+++ dropdown.js
@@ -0,0 +1,24 @@
+const dropdown = (selector = '.dropDown') => {
+	let sel;
+	const root = document.documentElement || window;
+	const clicktouch = ('ontouchstart' in root) ? 'touchstart' : 'click';
+	const clickOut = e => !sel.contains(e.target) && close();	
+	const keydown = e => !sel.contains(document.activeElement) && close();
+	const open = () => {
+		sel.querySelector('.summary').setAttribute('aria-expanded', true);
+		document.addEventListener('keyup', keydown, false);
+		window.addEventListener(clicktouch, clickOut);
+	}
+	const close = () =>  {
+		sel.querySelector('.summary').setAttribute('aria-expanded', false);
+		document.removeEventListener('keyup', keydown);
+		window.removeEventListener(clicktouch, clickOut);
+	}
+	document.querySelectorAll(selector).forEach(el => {
+		const btn = el.querySelector('.summary');
+		btn.onclick = () => {
+			sel = el;
+			btn.getAttribute('aria-expanded') === 'true' ? close() : open();
+		} 	
+	});
+}
