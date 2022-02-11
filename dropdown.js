424ae3bca2585838a7f5de5f750b486ca4207ba4
Jonathan MARTIN <j.martin@lonsdale.fr>
2022-02-11 11:30:39 +0100

    new

--- dropdown.js
+++ dropdown.js
@@ -5,62 +5,59 @@
  * 
  */
 
- const dropdown = (sel) => {
+ const dropdown = el => {
+	const focusable = 'a, button, input, textarea, select, details,[tabindex]:not([tabindex="-1"])';
 	const clicktouch = ('ontouchstart' in document.documentElement) ? 'touchstart' : 'click';
-	const clickOut = e => !sel.contains(e.target) && close();
-	const links = sel.querySelectorAll('a');
-	const summary = sel.querySelector('.summary');
-	let inc = -1;
+	const clickOut = e => !el.contains(e.target) && close();
+	const links = el.querySelectorAll(focusable);
+	const summary = el.querySelector('.summary');
 	let isOpen = false;
+	let inc = -1;
 	
-	const keydown = e => {
+	const focus = e => {
 		if(e.key === 'ArrowDown') inc++;
 		if(e.key === 'ArrowUp') inc--;
-		if(e.key === 'ArrowDown' || e.key === 'ArrowUp'){
+		if(e.key === 'Enter') inc = 0;
+		if(e.key === 'ArrowDown' || e.key === 'ArrowUp' || e.key === 'Enter'){
 			e.preventDefault();
 			!isOpen && open();
-			if(inc<0) inc = links.length-1;
-			if(inc>links.length-1) inc = 0;
+			if(inc < 0) inc = links.length-1;
+			if(inc > links.length-1) inc = 0;
 			links[inc].focus();
-		}
-		if(e.key === 'Enter'){
-			e.preventDefault();
-			!isOpen && open();
-		}
-		if(e.key === 'Tab'){
-			close();
-		}		
+		}	
+	}
+
+	const keydown = e => {
+		if(e.key === 'Tab') close();
 		if(e.key === 'Escape'){
 			summary.focus();
 			close();
 		}
-		!sel.contains(document.activeElement) && close();
+		!el.contains(document.activeElement) && close();
 	}
 
 	const open = () => {
 		isOpen = true;
 		summary.setAttribute('aria-expanded', true);
-		window.addEventListener(clicktouch, clickOut, true);
-		setTimeout(() => {
-			inc = 0;
-			links[0].focus();
-		},50);
+		window.addEventListener(clicktouch, clickOut);
+		document.addEventListener('keydown', keydown);
 	}
-
+	
 	const close = () =>  {
-		const ggg = document.querySelectorAll('button');
-		setTimeout(() => {
-			ggg.forEach((btn,i) => {
-				if(btn === summary) ggg[i+1].focus();
-			});
-		},500);
 		isOpen = false;
 		summary.setAttribute('aria-expanded', false);
 		document.removeEventListener('keydown', keydown);
+		document.removeEventListener('keydown', focus);
 		window.removeEventListener(clicktouch, clickOut);
-	}
-
-	summary.onfocus = () => document.addEventListener('keydown', keydown);
+		
+		// focus next element
+		const focusables = document.querySelectorAll(focusable);
+		const lastLink = links[links.length - 1];
+		focusables.forEach((link,i) => link == lastLink && focusables[i].focus());
+	};
+	
+	summary.onfocus = () => document.addEventListener('keydown', focus);
 	summary.onclick = () => summary.getAttribute('aria-expanded') === 'true' ? close() : open();
 }
+
 export default dropdown;
